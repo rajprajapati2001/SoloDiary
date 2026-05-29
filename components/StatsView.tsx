2cbdafb8da4721c0b324a6a625c2ca6d55de7bbf
rajprajapati2001 <rp5876907@gmail.com>
2026-05-29 12:32:30 +0530

    solodiary

--- components/StatsView.tsx
+++ components/StatsView.tsx
@@ -893,9 +893,9 @@ const openExternalLink = async () => {
   
 const openAndroidLink = async () => {
   try {
-    await Browser.open({ url: 'https://github.com/rajprajapati2001/SoloDiary/raw/refs/heads/main/android/app/build/outputs/apk/release/solo-diary-v.1.0.apk' });
+    await Browser.open({ url: 'https://github.com/rajprajapati2001/SoloDiary/raw/refs/heads/main/android/app/build/outputs/apk/release/app-release.apk' });
   } catch (error) {
-    window.open('https://github.com/rajprajapati2001/SoloDiary/raw/refs/heads/main/android/app/build/outputs/apk/release/solo-diary-v.1.0.apk', '_system');
+    window.open('https://github.com/rajprajapati2001/SoloDiary/raw/refs/heads/main/android/app/build/outputs/apk/release/app-release.apk', '_system');
     console.error("Browser plugin error:", error);
   }
 };
